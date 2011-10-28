59aad981f081f6170684e966f687b01caf88155a
Andrew Radev <andrey.radev@gmail.com>
2011-10-28 22:55:03 +0300

    Move some steps to cucumber-vimscript

--- features/step_definitions/splitjoin_steps.rb
+++ features/step_definitions/splitjoin_steps.rb
@@ -3,14 +3,6 @@ Given /^the splitjoin plugin is loaded$/ do
   puts @vim.add_plugin plugin_dir, 'plugin/splitjoin.vim'
 end
 
-Given /^"([^"]*)" is set$/ do |boolean_setting|
-  @vim.command("set #{boolean_setting}")
-end
-
-Given /^"([^"]*)" is set to "([^"]*)"$/ do |setting, value|
-  @vim.command("set #{setting}=#{value}")
-end
-
 When /^I split the line$/ do
   @vim.command 'SplitjoinSplit'
 end
