692f02dd9e84791957213317e5d46e3dd3c1bc1d
Andrew Radev <andrey.radev@gmail.com>
2011-11-12 17:46:11 +0200

    Minor fix

--- features/step_definitions/splitjoin_steps.rb
+++ features/step_definitions/splitjoin_steps.rb
@@ -1,6 +1,6 @@
 Given /^the splitjoin plugin is loaded$/ do
   plugin_dir = File.expand_path('../../..', __FILE__)
-  puts @vim.add_plugin plugin_dir, 'plugin/splitjoin.vim'
+  @vim.add_plugin plugin_dir, 'plugin/splitjoin.vim'
 end
 
 When /^I split the line$/ do
