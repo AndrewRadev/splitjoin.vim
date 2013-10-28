1def4623f4cfbcc220ad4c200738c074f0c0643d
Andrew Radev <andrey.radev@gmail.com>
2013-10-28 23:21:35 +0100

    Make go spec independent of filetype detection

--- spec/plugin/go_spec.rb
+++ spec/plugin/go_spec.rb
@@ -3,15 +3,18 @@ require 'spec_helper'
 describe "go" do
   let(:filename) { 'test.go' }
 
-  before :each do
-    vim.set 'expandtab'
-    vim.set 'shiftwidth', 4
+  # Go is not built-in, so let's set it up manually
+  def setup_go_filetype
+    vim.set(:filetype, 'go')
+    vim.set(:expandtab) # not canonical, but easier to test
+    vim.set(:shiftwidth, 4)
   end
 
   specify "structs" do
     set_file_contents <<-EOF
       StructType{one: 1, two: "asdf", three: []int{1, 2, 3}}
     EOF
+    setup_go_filetype
 
     split
 
