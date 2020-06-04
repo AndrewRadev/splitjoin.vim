5f78ebf3045bf21f8f1d9a1140a7a28a2b8770bf
Michel Belleville <michel.belleville@lifen.fr>
2020-06-04 22:23:29 +0200

    new approach: no parser, using the vim matching capabilities

--- autoload/sj/elm.vim
+++ autoload/sj/elm.vim
@@ -1,24 +1,80 @@
 function! sj#elm#SplitList()
   let [from, to] = sj#LocateBracesAroundCursor('[', ']')
 
-  echomsg "from = " from
-  echomsg "to = " to
-
   if from < 0
     return 0
   endif
 
-  let [from, to, args] = sj#argparser#elm#ParseArguments(from + 1, to - 1, getline('.'))
-
-  if from < 0
-    return 0
-  endif
+  let args = sj#elm#ListArgs(from, to)
 
   let replacement = join(args, "\n, ")
 
-  let replacement = " ".replacement."\n"
+  let replacement = "[ ".replacement."\n]"
   call sj#ReplaceCols(from, to, replacement)
 
   return 1
 endfunction
 
+function sj#elm#ListArgs(from, to)
+  let bufferBefore = @@
+
+  call cursor(line('.'), a:from + 1)
+
+  let args = []
+  let arg = ""
+
+  while col('.') < a:to
+    let character = sj#elm#CurrentChar()
+    if character == ","
+      if len(arg) > 0
+        call add(args, arg)
+        let arg = ""
+      endif
+      call cursor(line('.'), col('.') + 1)
+    elseif character =~ "[\"'{\[(]"
+      let arg = sj#elm#AddToArgAndGetToNextWord(arg, sj#elm#CaptureMatching(character))
+    else
+      let arg = sj#elm#AddToArgAndGetToNextWord(arg, sj#elm#CaptureWord())
+    endif
+  endwhile
+
+  if len(arg) > 0
+    call add(args, arg)
+  endif
+
+  let @@ = bufferBefore
+
+  return args
+endfunction
+
+function sj#elm#CaptureMatching(character)
+  execute "normal! ya" . a:character
+  return @@
+endfunction
+
+function sj#elm#CaptureWord()
+  normal! yiw
+  return @@
+endfunction
+
+function sj#elm#CurrentChar()
+  return getline('.')[col('.') - 1]
+endfunction
+
+function sj#elm#AddToArgAndGetToNextWord(arg, newPart)
+  let newArg = sj#Trim(sj#elm#AddToArg(a:arg, a:newPart))
+  call cursor(line('.'), col('.') + len(a:newPart))
+  return newArg
+endfunction
+
+function sj#elm#AddToArg(arg, newPart)
+  if a:arg == ""
+    return a:newPart
+  endif
+
+  if a:newPart == "." || a:arg =~ "\\.$"
+    return join([a:arg, a:newPart], "")
+  else
+
+  return join([a:arg, a:newPart], " ")
+endfunction
--- spec/plugin/elm_spec.rb
+++ spec/plugin/elm_spec.rb
@@ -15,7 +15,7 @@ describe 'elm' do
       specify 'with a simple list' do
         set_file_contents <<~EOF
           list =
-              [1, 2, 3, 4]
+              [1, 22, 333, 4444]
         EOF
 
         vim.search '['
@@ -24,12 +24,120 @@ describe 'elm' do
         assert_file_contents <<~EOF
           list =
               [ 1
-              , 2
-              , 3
-              , 4
+              , 22
+              , 333
+              , 4444
               ]
         EOF
       end
+
+      specify 'with a space and tab-riddled list' do
+        set_file_contents <<~EOF
+          list =
+              [ \t1\t , \t22,\t 333 , \t4444\t ]
+        EOF
+
+        vim.search '['
+        split
+
+        assert_file_contents <<~EOF
+          list =
+              [ 1
+              , 22
+              , 333
+              , 4444
+              ]
+        EOF
+      end
+
+      specify 'with a list holding function call results' do
+        set_file_contents <<~EOF
+          list =
+              [1 + 2, modBy 3 4, remBy 5 (num + 6), maybeNum |> Maybe.withDefault 7]
+        EOF
+
+        vim.search '['
+        split
+
+        assert_file_contents <<~EOF
+          list =
+              [ 1 + 2
+              , modBy 3 4
+              , remBy 5 (num + 6)
+              , maybeNum |> Maybe.withDefault 7
+              ]
+        EOF
+      end
+
+      specify 'with a list of lists' do
+        set_file_contents <<~EOF
+          list =
+              [[123, 456], [78, 89, 90]]
+        EOF
+
+        vim.search '['
+        split
+
+        assert_file_contents <<~EOF
+          list =
+              [ [123, 456]
+              , [78, 89, 90]
+              ]
+        EOF
+      end
+
+      specify 'with a list of tuples' do
+        set_file_contents <<~EOF
+          list =
+              [(123, 456), (78, 89)]
+        EOF
+
+        vim.search '['
+        split
+
+        assert_file_contents <<~EOF
+          list =
+              [ (123, 456)
+              , (78, 89)
+              ]
+        EOF
+      end
+
+      specify 'with a list of messy strings' do
+        set_file_contents <<~EOF
+          list =
+              ["One, two", "\\"One, two\\", \\"three, four\\""]
+        EOF
+
+        vim.search '['
+        split
+
+        assert_file_contents <<~EOF
+          list =
+              [ "One, two"
+              , "\\"One, two\\", \\"three, four\\""
+              ]
+        EOF
+      end
+    end
+
+    xspecify 'joining a list' do
+      set_file_contents <<~EOF
+        list =
+            [ 1
+            , 2
+            , 3
+            , 4
+            ]
+      EOF
+
+      vim.search '['
+      join
+
+      assert_file_contents <<~EOF
+        list =
+          [1, 2, 3, 4]
+      EOF
     end
   end
 end
